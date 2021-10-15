b5f1518a8d8c9ed971069fc8f5ecc689a64bc9a0
Kyrian <dr.kyrian.onwe@gmail.com>
2021-10-14 19:46:45 -0500

    Completed assignment.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2= 2;
+const multiply =  num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+let num3 = 25;
+let num4 = 7;
+const mod = num3 % num4;
+
+
+let numSet = (5.7, 4, 12, 8, 3, 19.9, 20);
+const max = Math.max(numSet);
